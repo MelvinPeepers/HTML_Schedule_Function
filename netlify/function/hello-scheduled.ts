1fd5332a5f7d617a2325ff058eb46cef760cfb58
Melvin Peepers <melvinpeepers@Mac.lan>
2025-04-15 12:57:03 -0500

    hello scheduled function

---
+++ netlify/function/hello-scheduled.ts
@@ -0,0 +1,13 @@
+import type { Config } from "@netlify/functions"
+
+export default async function handler(req: Request): Promise<Response> {
+  const now = new Date().toISOString()
+  console.log(`[Scheduled Function] Hello! The time is: ${now}`)
+
+  return new Response("Scheduled function ran successfully")
+}
+
+// Run every hour
+export const config: Config = {
+  schedule: "@hourly"
+}
