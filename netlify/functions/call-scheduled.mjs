3ae2e6aa5aaaef6d7200b5346ebdff25c08dcc49
Melvin Peepers <melvinpeepers@Mac.lan>
2025-04-18 10:05:51 -0500

    update Scheduled fucntion call-scheduled

--- netlify/functions/call-scheduled.mjs
+++ netlify/functions/call-scheduled.mjs
@@ -1,17 +1,17 @@
 import type { Config } from "@netlify/functions"
 
-export default async function handler(req) {
-    // Call my wife
-    console.log("Calling wife... 💖");
-  
-    return new Response("Called wife!");
-  }
-  
-  export const config = {
-    schedule: [
-      "0 0 * * *",   // 00:00 UTC
-      "0 6 * * *",   // 06:00 UTC
-      "0 12 * * *",  // 12:00 UTC
-      "0 18 * * *"   // 18:00 UTC
-    ]
-  };
+export default async function handler() {
+  // Call my wife
+  console.log("Calling wife... 💖");
+
+  return new Response("Called wife!");
+}
+
+export const config: Config = {
+  schedule: [
+    "0 0 * * *",   // 00:00 UTC
+    "0 6 * * *",   // 06:00 UTC
+    "0 12 * * *",  // 12:00 UTC
+    "0 18 * * *"   // 18:00 UTC
+  ]
+};
