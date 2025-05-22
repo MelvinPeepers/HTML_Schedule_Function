a72294cdfeb8f1703c8a34d0b74b174a3ac83948
Melvin Peepers <melvinpeepers@Mac.lan>
2025-05-22 12:47:12 -0500

    updated function to mjs

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,9 +1,8 @@
-exports.handler = async (event, context) => {
+export async function handler(event, context) {
     console.log("Function execution");
     console.log("Function started");
-    await new Promise ((resolve) => setTimeout(resolve, 3500)); // 500ms delay
+    await new Promise ((resolve) => setTimeout(resolve, 6500)); // 500ms delay
     console.log("Function completed");
-    console.log("Function execution");
   return {
     statusCode: 200,
     body: JSON.stringify({
